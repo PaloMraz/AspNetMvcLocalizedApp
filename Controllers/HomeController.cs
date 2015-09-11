c0a2f27cf690b6332de9c8fcbabb168731ea3dd9
PaloMraz <palo@lamarvin.com>
2015-09-11 08:48:35 +0200

    Initial commit.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using AspNetMvcLocalizedApp.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcLocalizedApp.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      this.ViewBag.Title = Strings.Common_AppName;
+      return this.View();
+    }
+
+
+    public ActionResult ChooseCulture()
+    {
+      return this.View(Infrastructure.LocalizationFilterAttribute.GetSupportedCultures());
+    }
+
+
+    [HttpPost]
+    public ActionResult ChooseCulture(string selectedCulture)
+    {
+      Infrastructure.LocalizationFilterAttribute.SetCurrentCultureCookie(this.Response, selectedCulture);
+      return this.RedirectToAction("Index");
+    }
+  }
+}
---
+++ Infrastructure/LocalizationFilterAttribute.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcLocalizedApp.Infrastructure
+{
+  public class LocalizationFilterAttribute : ActionFilterAttribute
+  {
+    private const string CultureCookieName = "_culture";
+
+    private static SelectListItem[] _s_supportedCultures =
+    {
+      new SelectListItem() { Value = "en-US", Text = "English" },
+      new SelectListItem() { Value="sk-SK", Text = "Slovensky" }
+    };
+
+
+    public LocalizationFilterAttribute()
+    { }
+
+
+    public static List<SelectListItem> GetSupportedCultures()
+    {
+      string currentCulture = Thread.CurrentThread.CurrentUICulture.Name;
+      return _s_supportedCultures
+        .Select(item => new SelectListItem() { Value = item.Value, Text = item.Text, Selected = item.Value == currentCulture })
+        .ToList();
+    }
+
+
+    public static void SetCurrentCultureCookie(HttpResponseBase response, string cultureName)
+    {
+      if (_s_supportedCultures.Any(item => item.Value == cultureName))
+      {
+        response.SetCookie(new HttpCookie(CultureCookieName, cultureName) { HttpOnly = true });
+      }
+    }
+
+
+    public override void OnActionExecuting(ActionExecutingContext filterContext)
+    {
+      base.OnActionExecuting(filterContext);
+
+      if (filterContext.IsChildAction)
+      {
+        return;
+      }
+
+      HttpRequestBase request = filterContext.HttpContext.Request;
+      HttpCookie cultureCookie = request.Cookies.Get(CultureCookieName);
+      if (cultureCookie == null)
+      {
+        cultureCookie = new HttpCookie(CultureCookieName, _s_supportedCultures[0].Value)
+        {
+          HttpOnly = true
+        };
+
+        // Culture cookie namáme - pozrieme, čo chce browser.
+        if (request.UserLanguages != null)
+        {
+          string firstSupportedCulture = request.UserLanguages
+            .Intersect(_s_supportedCultures.Select(item => item.Value))
+            .FirstOrDefault();
+
+          if (firstSupportedCulture != null)
+          {
+            cultureCookie.Value = firstSupportedCulture;
+          }
+        }
+        filterContext.HttpContext.Response.SetCookie(cultureCookie);
+      }
+
+      Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(cultureCookie.Value);
+      Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+    }
+  }
+}
